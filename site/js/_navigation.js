bf5725fa61e388b071ed6d1ed0392598195416ba
ParsedClassics <soladevs@gmail.com>
2024-01-31 00:08:08 +0200

    [130]Feature: Add Latin Morphology drills pages

--- site/js/_navigation.js
+++ site/js/_navigation.js
@@ -18,12 +18,14 @@ $(document).ready(function(){
 
     '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding" href="../docs/for_reader.html">Reader\'s docs</a>' + 
 
+    '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding" href="../site/drills.html">Drills</a>' + 
+
     '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding" href="../site/copyright.html">Copyright</a>' + 
 
     '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding" href="../site/contacts.html">Contacts</a>' + 
     
     '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding" href="../site/about.html">About</a>' + 
-    // IMPORTANT! Editor's tools page must be inside /site/ directory!
+
     '<a class="w3-bar-item w3-hover-dark-grey w3-button w3-padding pc-small pc-position-absolute pc-bottom-0" href="../tools/index.html">Editor\'s area</a>' +
 
     '</div>');
