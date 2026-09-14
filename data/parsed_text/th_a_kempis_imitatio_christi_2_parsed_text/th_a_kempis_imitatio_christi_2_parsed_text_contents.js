2a284fd00f4aa709a822298dd742f58bf6552345
ParsedClassics <soladevs@gmail.com>
2026-09-14 13:36:29 +0300

    [819]Add Thomae a Kempis Imitatio Christi as Parsed text resources Part 2

---
+++ data/parsed_text/th_a_kempis_imitatio_christi_2_parsed_text/th_a_kempis_imitatio_christi_2_parsed_text_contents.js
@@ -0,0 +1,189 @@
+/*
+=====================================================
+ This file is part of ParsedClassics app
+=====================================================
+ Copyright (c) Éleuthère Ioannidis
+=====================================================
+*/
+
+th_a_kempis_imitatio_christi_2_parsed_text_contents = {
+
+"title": "Titulus"
+
+, "levelstart-1": "Incipit liber de interna conversationes."
+
+, "I:1": "I:1"
+
+, "I:2": "I:2"
+
+, "I:3": "I:3"
+
+, "I:4": "I:4"
+
+, "I:5": "I:5"
+
+, "I:6": "I:6"
+
+, "I:7": "I:7"
+
+, "I:8": "I:8"
+
+, "levelend-1": ""
+
+, "levelstart-2": "De submissione, Praelati regimine."
+
+, "II:1": "II:1"
+
+, "II:2": "II:2"
+
+, "levelend-2": ""
+
+, "levelstart-3": "De bono pacifico homine."
+
+, "III:1": "III:1"
+
+, "III:2": "III:2"
+
+, "III:3": "III:3"
+
+, "levelend-3": ""
+
+, "levelstart-4": "De pura mente et simplici intentione."
+
+, "IV:1": "IV:1"
+
+, "IV:2": "IV:2"
+
+, "IV:3": "IV:3"
+
+, "levelend-4": ""
+
+, "levelstart-5": "De propria consideratione."
+
+, "V:1": "V:1"
+
+, "V:2": "V:2"
+
+, "V:3": "V:3"
+
+, "levelend-5": ""
+
+, "levelstart-6": "De laetitia bonae conscientiae."
+
+, "VI:1": "VI:1"
+
+, "VI:2": "VI:2"
+
+, "VI:3": "VI:3"
+
+, "VI:4": "VI:4"
+
+, "levelend-6": ""
+
+, "levelstart-7": "De amore Jesu super omnia."
+
+, "VII:1": "VII:1"
+
+, "VII:2": "VII:2"
+
+, "VII:3": "VII:3"
+
+, "levelend-7": ""
+
+, "levelstart-8": "De familiari amicitia Jesu."
+
+, "VIII:1": "VIII:1"
+
+, "VIII:2": "VIII:2"
+
+, "VIII:3": "VIII:3"
+
+, "VIII:4": "VIII:4"
+
+, "VIII:5": "VIII:5"
+
+, "levelend-8": ""
+
+, "levelstart-9": "De carentia omnis solatii."
+
+, "IX:1": "IX:1"
+
+, "IX:2": "IX:2"
+
+, "IX:3": "IX:3"
+
+, "IX:4": "IX:4"
+
+, "IX:5": "IX:5"
+
+, "IX:6": "IX:6"
+
+, "IX:7": "IX:7"
+
+, "IX:8": "IX:8"
+
+, "levelend-9": ""
+
+, "levelstart-10": "De gratitudine progratia Dei."
+
+, "X:1": "X:1"
+
+, "X:2": "X:2"
+
+, "X:3": "X:3"
+
+, "X:4": "X:4"
+
+, "X:5": "X:5"
+
+, "levelend-10": ""
+
+, "levelstart-11": "De paucitate amatorum crucis."
+
+, "XI:1": "XI:1"
+
+, "XI:2": "XI:2"
+
+, "XI:3": "XI:3"
+
+, "XI:4": "XI:4"
+
+, "XI:5": "XI:5"
+
+, "levelend-11": ""
+
+, "levelstart-12": "De regia via sanctae crucis."
+
+, "XII:1": "XII:1"
+
+, "XII:2": "XII:2"
+
+, "XII:3": "XII:3"
+
+, "XII:4": "XII:4"
+
+, "XII:5": "XII:5"
+
+, "XII:6": "XII:6"
+
+, "XII:7": "XII:7"
+
+, "XII:8": "XII:8"
+
+, "XII:9": "XII:9"
+
+, "XII:10": "XII:10"
+
+, "XII:11": "XII:11"
+
+, "XII:12": "XII:12"
+
+, "XII:13": "XII:13"
+
+, "XII:14": "XII:14"
+
+, "XII:15": "XII:15"
+
+, "levelend-12": ""
+
+};
